a186fb82a2824d7c8e62a32ad5b04e32d46b6299
Danny Quarton <danny@agidea.uk>
2020-01-09 16:01:24 +0000

    Update AssemblyInfos and create nuspec files

--- Umbraco.Storage.S3/Umbraco.Storage.S3.Media/Properties/AssemblyInfo.cs
+++ Umbraco.Storage.S3/Umbraco.Storage.S3.Media/Properties/AssemblyInfo.cs
@@ -2,35 +2,32 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Umbraco.Storage.S3.Forms")]
-[assembly: AssemblyDescription("AWS S3 Filesystem Provider for Umbraco Forms")]
+[assembly: AssemblyTitle("Umbraco.Storage.S3.Media")]
+[assembly: AssemblyDescription("AWS S3 Filesystem Provider for Umbraco media items")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Danny Quarton")]
-[assembly: AssemblyProduct("Umbraco.Storage.S3.Forms")]
+[assembly: AssemblyCompany("Elijah Glover, Neil Gaietto, Danny Quarton")]
+[assembly: AssemblyProduct("Umbraco.Storage.S3.Media")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("40dfeea8-aabb-47ed-a8a9-e5465926e126")]
+[assembly: Guid("65c9396a-95f7-4e76-9539-b7be3e9ae584")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("8.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- Umbraco.Storage.S3/Umbraco.Storage.S3/Properties/AssemblyInfo.cs
+++ Umbraco.Storage.S3/Umbraco.Storage.S3/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Umbraco.Storage.S3")]
-[assembly: AssemblyDescription("Umbraco S3 Filesystem Provider")]
+[assembly: AssemblyTitle("Umbraco.Storage.S3.Core")]
+[assembly: AssemblyDescription("Base AWS S3 Filesystem provider for Umbraco. Required by Umbraco.Storage.S3.Forms and Umbraco.Storage.S3.Media")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Elijah Glover")]
-[assembly: AssemblyProduct("Umbraco.Storage.S3")]
+[assembly: AssemblyCompany("Elijah Glover, Neil Gaietto, Danny Quarton")]
+[assembly: AssemblyProduct("Umbraco.Storage.S3.Core")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("8.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
